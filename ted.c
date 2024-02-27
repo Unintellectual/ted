c4d02616ccc795b53690631602f4bca097681886
Noah Victoriano <noahvictoriano0@gmail.com>
2024-02-27 20:40:48 +0800

    added CTRL Q to quit function

--- ted.c
+++ ted.c
@@ -1,59 +1,87 @@
 /*** Includes ***/
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <termios.h>
 #include <ctype.h>
-#include <stdio.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** Defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** Data ***/
 struct termios orig_termios;
 
 /*** Terminal ***/
-void die (const char *s) {
-	perror(s);
-	exit(1);
+void die(const char *s) {
+  perror(s);
+  exit(1);
 }
 
-void disableRawMode(){
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-		die("tcsetattr");
-
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
-void enableRawMode(){
 
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) 
-		die("tcgetattr");
-	atexit(disableRawMode);
+void enableRawMode() {
 
-	struct termios raw = orig_termios;
-	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-	raw.c_oflag &= ~(OPOST);
-	raw.c_cflag |= (CS8);
-	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
-	raw.c_cc[VMIN] = 0;
-	raw.c_cc[VMIN] = 1;
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    die("tcgetattr");
+  atexit(disableRawMode);
+  // disable CTRL + (input keys)
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VMIN] = 1;
 
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
-		die("tcsetattr");
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    die("tcsetattr");
+}
+
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN)
+      die("read");
+  }
+  return c;
+}
+
+/*** input ***/
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+  case CTRL_KEY('q'):
+    exit(0);
+    break;
+  }
 }
 
 /*** Init ***/
 int main() {
   enableRawMode();
+
+  while (1) {
+    editorProcessKeypress();
+  }
+
   while (1) {
     char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) 
-    	die("read");
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+      die("read");
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
-    if (c == 'q') break;
+    if (c == CTRL_KEY('q'))
+      break;
   }
   return 0;
 }
-
-
