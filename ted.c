8456ca27c0928a55a1d307a2c0743ffaae2eec2c
Noah Victoriano <noahvictoriano0@gmail.com>
2024-03-01 00:26:03 +0800

    cleaned a bunch of thangs

--- ted.c
+++ ted.c
@@ -1013,13 +1013,19 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.dirty = 0;
+  E.filename = NULL;
+  E.statusmsg[0] = '\0';
+  E.statusmsg_time = 0;
+  E.syntax = NULL;
 
-  if (getWindowSize(&E.screenRows, &E.screenCols) == -1)
-    die("getWindowSize");
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+  E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[]) {
@@ -1029,9 +1035,12 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
+  editorSetStatusMessage(
+    "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
+
   while (1) {
-    editorProcessKeypress();
     editorRefreshScreen();
+    editorProcessKeypress();
   }
 
   return 0;
